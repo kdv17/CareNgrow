fd05601cd55e0e83eb0ce0e410173bd9465a2c31
kdv17 <143061964+kdv17@users.noreply.github.com>
2024-03-20 19:50:47 +0530

    Add files via upload

---
+++ dti project/script.js
@@ -0,0 +1,19 @@
+document.getElementById('meditationButton').addEventListener('click', function() {
+    // Handle click action for meditation button
+});
+
+document.getElementById('yogaButton').addEventListener('click', function() {
+    // Handle click action for yoga button
+});
+
+document.getElementById('taskButton').addEventListener('click', function() {
+    // Handle click action for task button
+});
+
+document.getElementById('moodButton').addEventListener('click', function() {
+    // Handle click action for mood button
+});
+
+document.getElementById('settingsButton').addEventListener('click', function() {
+    // Handle click action for settings button
+});
